629386681e941a030c3ea4653bf836f354d75be0
Funyn <461547449@qq.com>
2018-03-17 14:27:41 +0800

    优化了博客页面

---
+++ www/config.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import config_default
+
+class Dict(dict):
+    """
+    自定义字典，但是存在x.y的风格
+    """
+    def __init__(self,names=(),values=(),**kw):
+        super(Dict, self).__init__(**kw)
+        for k,v in zip(names,values):
+            self[k] = v
+
+    def __getattr__(self, item):
+        try:
+            return self[item]
+        except KeyError:
+            raise AttributeError('Dict object has no attributes %s'%item)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+def merge(defaults,override):
+    r = {}
+    for k,v in defaults.items():
+        if k in override:
+            if isinstance(v,dict):
+                r[k] = merge(v,override[k])
+            else:
+                r[k] = override[k]
+        else:
+            r[k] = v
+    return r
+
+def toDict(d):
+    D = Dict()
+    for k,v in d.items():
+        D[k] = toDict(v) if isinstance(v,dict) else v
+    return D
+
+configs = config_default.configs
+
+try:
+    import config_override
+    configs = merge(configs,config_override.configs)
+except ImportError:
+    pass
+
+configs = toDict(configs)
---
+++ www/config_default.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+configs = {
+    'debug':True,
+    'db':{
+        'host':'127.0.0.1',
+        "port":3306,
+        "user":'root',
+        "password":'123456',
+        "db":'fyf'
+    },
+    "sessions":{
+        "secret":'youtothink'
+    }
+}
+
+
+# configs = {
+#     'debug':True,
+#     'db':{
+#         'host':'127.0.0.1',
+#         "port":3306,
+#         "user":'admin',
+#         "password":'Root110qwe',
+#         "db":'awesome_blog'
+#     },
+#     "sessions":{
+#         "secret":'youtothink'
+#     }
+# }
